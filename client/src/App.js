b63780c702759f72156a4453135bdb3efe39de3c
jynt1401 <jayantbelwanshi1401@gmail.com>
2023-04-07 18:18:16 +0530

    login form

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,7 @@
 import "./App.css";
-// import { useState, useEffect } from 'react';
+// import { useState} from 'react';
 // import Axios from "axios";
-import { createContext } from "react";
-import { GoogleOAuthProvider } from "@react-oauth/google";
+import { createContext, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Home/Landing";
 import Profile from "./pages/Profile";
@@ -13,15 +12,21 @@ import CatBook from "./pages/Home/CatBook";
 import BookDetails from "./pages/Home/BookDetails/BookDetails"
 
 
+
 export const Context = createContext();
 
 function App() {
+
+
   return (
     <Context.Provider value={{}}>
-      <GoogleOAuthProvider clientId="959469014856-evue44rqpagjru2pe34irb4mvlk68s03.apps.googleusercontent.com">
+      
         <div id="App">
+          
+        
           <Router>
-            <Nav />
+            <Nav/>
+            
             <Routes>
               <Route exact path="/" element={<Landing />} />
               <Route exact path="/PublishBook" element={<PublishBook />} />
@@ -32,7 +37,7 @@ function App() {
             </Routes>
           </Router>
         </div>
-      </GoogleOAuthProvider>
+      
     </Context.Provider>
   );
 }
